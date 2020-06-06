aa91cd2208de27180a74979a9ae726143bf5633d
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-06-06 16:54:21 +0500

    Login & Logout Done

--- SqBlog/users/urls.py
+++ SqBlog/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .import views
+from django.contrib.auth import views as aut_views
 urlpatterns = [
     path('',views.register , name='user-register'),
-    # path('about',views.about , name='blog-about'),
+    path('login/',aut_views.LoginView.as_view(template_name='users/login.html') , name='login'),
+    path('logout/',aut_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/',views.profile , name='profile'),
 ]
