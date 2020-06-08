c99cc5274dd03e40d594fe9e7cd7f7a1541b3d28
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-06-08 20:10:53 +0500

    profile update done

--- SqBlog/SqBlog/urls.py
+++ SqBlog/SqBlog/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 # from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +29,10 @@ urlpatterns = [
 
 
 ]
+urlpatterns=urlpatterns + static(settings.MEDIA_URL ,  document_root = settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns +=
+
+
+
--- SqBlog/users/views.py
+++ SqBlog/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -24,4 +24,34 @@ def register(request):
 
 @login_required()
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method=="POST":
+        u_form=UserUpdateForm(request.POST,instance=request.user)
+        p_form=ProfileUpdateForm(request.POST,
+                                 request.FILES,
+                                 instance=request.user.profile)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your Personal Info has been successfully Updated !')
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request, f'Your Profile Image has been successfully Updated !')
+
+
+            return redirect('profile')
+        # else:
+        #     messages.error(request,f'Please Fill Carefully with Instructions ')
+
+
+    else:
+        u_form = UserUpdateForm( instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+
+    context={
+        'u_form':u_form,
+        'p_form':p_form,
+    }
+    return render(request,'users/profile.html',context)
+
+
